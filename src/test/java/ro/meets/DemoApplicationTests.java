1203e84c0227d86ba211ffd420f6f577fc6ec1ca
Rohit Nair <rohitcse081@gmail.com>
2025-08-19 21:23:35 +0530

    Sprint Setup Commit

---
+++ src/test/java/ro/meets/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.meets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
